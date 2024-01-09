ea9ebfa85e1ebe15af8da56b4874da016fe0e646
Dinesh Shrestha <dinesh.shrestha@mat-norge.no>
2024-01-09 15:07:39 +0100

    Get a User's post

--- lib/forum_web/views/user_view.ex
+++ lib/forum_web/views/user_view.ex
@@ -1,6 +1,7 @@
 defmodule ForumWeb.UserView do
   use ForumWeb, :view
   alias ForumWeb.UserView
+  alias Forum.Posts.Post
 
   def render("index.json", %{users: users}) do
     %{data: render_many(users, UserView, "user.json")}
@@ -11,6 +12,15 @@ defmodule ForumWeb.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id, name: user.name, email: user.email}
+    %{
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      posts: for(post <- user.posts, do: post_data(post))
+    }
+  end
+
+  def post_data(%Post{} = post) do
+    %{id: post.id, title: post.title, body: post.body}
   end
 end
